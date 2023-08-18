8efdff015f628d25d9cf93af2fdd31893ae347c6
Vivek <vivek.s@codebrewinnovations.com>
2023-08-18 16:15:37 +0530

    resolver-fix

--- Backend/src/resolvers/index.ts
+++ Backend/src/resolvers/index.ts
@@ -3,9 +3,6 @@ import UserResolver from './userResolvers';
 import TaskResolver from './taskResolvers';
 
 // Combine all the resolvers into a single object
-const resolvers = {
-    ...UserResolver,
-    ...TaskResolver,
-};
+const resolvers = [UserResolver,TaskResolver]
 
 export default resolvers;
